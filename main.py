9ee042268071a8f75a13c329ce66c251febb4122
alesdolenec <ales.dolenec@elea.si>
2021-10-10 16:16:37 +0200

    initial Upload

---
+++ main.py
@@ -0,0 +1,36 @@
+# # intigers (cela števila)
+# x = 5
+# y = 3
+# z = 77
+#
+# # Float
+# a = 10.74
+# print(x + y + z +a)
+#
+# # string
+# ime = "Aleš"
+# vreme = "Danes je lep dan!"
+# print(ime + " "+ vreme)
+#
+# # bollean
+# dezuje = False
+#
+# # nothing
+# poplave = None
+
+# name = input("What's your name?")
+# print("Hello, " + name +"!" )
+
+# prvaSt = int(input("Vnesi prvo številko: "))
+# drugaSt = int(input("Vnesi drugo številko: "))
+#
+# print(prvaSt + drugaSt)
+
+mood = input("What's your mood today? ")
+
+if mood == "Happy":
+  print("Its great to se you happy!")
+elif mood == "Nervous":
+  print("Take a deep breath 3 times")
+else:
+  print("cheer up, mate")
